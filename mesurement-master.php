eb4c55ee4f045af6238a2b1ab15e17c9fdc77311
Nityasaha13 <nityasaha39@gmail.com>
2024-05-12 00:45:04 +0530

    Add admin notice when plugin activates

--- mesurement-master.php
+++ mesurement-master.php
@@ -10,12 +10,35 @@ Author: Nitya Saha
 register_activation_hook(__FILE__, 'measurement_master_activate');
 function measurement_master_activate()
 {
+    // Create transient data for activation notice
+    set_transient('measurement-master-activation-notice', true, 5);
 }
 
 register_deactivation_hook(__FILE__, 'measurement_master_deactivate');
 function measurement_master_deactivate()
 {
 }
+
+
+// Add admin notice
+add_action('admin_notices', 'measurement_master_activate_admin_notice');
+
+/**
+ * Admin Notice on Activation.
+ */
+function measurement_master_activate_admin_notice() {
+    // Check transient, if available display notice
+    if (get_transient('measurement-master-activation-notice')) {
+        ?>
+        <div class="updated notice is-dismissible">
+            <p><?php esc_html_e('Enable the measurement settings plugin from Measurement Master settings page.', 'plugin-text-domain'); ?></p>
+        </div>
+        <?php
+        // Delete transient, only display this notice once
+        delete_transient('measurement-master-activation-notice');
+    }
+}
+
 
 //Check the functionality is on or not, if on than it will show in the product page
 $plugin_enabled = get_option('enable_plugin');
